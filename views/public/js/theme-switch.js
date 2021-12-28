5896e637b0ec2b5883c2dd1b33706b81ba04ab69
alex_pish <pishulev23071999@gmail.com>
2021-12-28 18:14:09 +0400

    Make dark mode default

--- views/public/js/theme-switch.js
+++ views/public/js/theme-switch.js
@@ -4,13 +4,21 @@ const DARK_BACKGROUND = '#222';
 const WHITE_BACKGROUND = '#fff';
 
 const themeStyle = document.getElementById('theme-style');
-const isDarkMode = localStorage.getItem('darkSwitch') !== null && localStorage.getItem('darkSwitch') === 'dark';
+const isDarkMode = localStorage.getItem('lightMode') === null || localStorage.getItem('lightMode') !== 'enabled';
+
+function setBackgroundColor(isDarkMode) {
+  if (isDarkMode) {
+    document.documentElement.style.backgroundColor = DARK_BACKGROUND;
+  } else {
+    document.documentElement.style.backgroundColor = WHITE_BACKGROUND;
+  }
+}
 setBackgroundColor(isDarkMode);
 
 window.addEventListener('load', function () {
   let darkSwitch = document.getElementById('darkSwitch');
   if (darkSwitch) {
-    darkSwitch.checked = localStorage.getItem('darkSwitch') !== null && localStorage.getItem('darkSwitch') === 'dark';
+    darkSwitch.checked = isDarkMode;
     darkSwitch.checked ? activateDark() : deactivateDark();
     darkSwitch.addEventListener('change', function () {
       darkSwitch.checked ? activateDark() : deactivateDark();
@@ -22,20 +30,12 @@ function activateDark() {
   setBackgroundColor(true);
   themeStyle.setAttribute('href', DARK_THEME_PATH);
   document.body.setAttribute('data-theme', 'dark');
-  localStorage.setItem('darkSwitch', 'dark');
+  localStorage.removeItem('lightMode');
 }
 
 function deactivateDark() {
   setBackgroundColor(false);
   themeStyle.setAttribute('href', WHITE_THEME_PATH);
   document.body.removeAttribute('data-theme');
-  localStorage.removeItem('darkSwitch');
-}
-
-function setBackgroundColor(isDarkMode) {
-  if (isDarkMode) {
-    document.documentElement.style.backgroundColor = DARK_BACKGROUND;
-  } else {
-    document.documentElement.style.backgroundColor = WHITE_BACKGROUND;
-  }
+  localStorage.setItem('lightMode', 'enabled');
 }
